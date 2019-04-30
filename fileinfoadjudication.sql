032b21d7880eba9e104d371ff9ff25084b62ee2d
Kevin Liu - A-Check <kliu@acheck.local>
2019-04-30 16:47:48 -0700

    Add parameter dataset

--- fileinfoadjudication.sql
+++ fileinfoadjudication.sql
@@ -1,11 +1,12 @@
---declare @startDate datetime
---declare @endDate datetime, @site varchar(20)
---set @startDate = '2017-05-01'
---set @endDate = '2017-05-15'
---set @site='SPRI001-0000'
-
+declare @startDate datetime
+declare @endDate datetime, @site varchar(20),@Batchno varchar(50)
+set @startDate = '2017-05-01'
+set @endDate = '2017-05-15'
+set @site='SPRI001-0000'
+--set @Batchno='SPRI001-0000'
 SELECT DISTINCT 
      bg.csiteno AS [Site#], 
+     bg.cbatchno, 
 	 bg.cfileno AS [File#], 
 	 bg.cappfname AS [First Name], 
 	 bg.capplname AS [Last Name], 
@@ -36,9 +37,10 @@ FROM            bgdrequest AS bg
 where bg.dcompdate > @startDate
 and bg.dcompdate < dateadd(d, 1, @endDate)
 and bg.csiteno in (@site)
+and bg.cbatchno in (@Batchno)
 and bg.cstatus='CLOSED'
 
-select Site#, File#, [First Name], [Last Name], 
+select [Site#], cbatchno [Batchno],File#, [First Name], [Last Name], 
  [date requested][Date Requested]
 , [date completed][Date Completed]
 ,[Requestor],[File Status], 
@@ -53,3 +55,42 @@ flagdescription, [Current Adj. Date]
 from #Temp
 
 drop table #temp
+
+
+
+/*parameter data set */
+-- industry
+select distinct clnofbus[Industry] , clnofbus[IndustryKey]
+from client
+where csiteno not like 'PSTS%'
+and csiteno not like 'TEST%'
+and lactive=1
+and clnofbus <> ''
+union all
+select 'BLANK', ''
+order by 1
+--client
+DECLARE @Group varchar(55)
+SELECT @Group = CASE WHEN @Groupheader IS NULL THEN 'ALL' ELSE @Groupheader END
+SELECT DISTINCT cgrpno AS [Client] , cgrpno as GroupKey
+FROM [dbo].[Client] A
+where (cgrpno in (@Groupheader)
+or left(cgrpno, 4) in (@groupheader)
+OR @Group='ALL')
+AND clnofbus IN (@Industry)
+ORDER BY cgrpno
+--corpid
+SELECT DISTINCT  cCorpID[CorpID]
+FROM [dbo].[Client]
+WHERE  cgrpno IN (@Client)
+AND clnofbus IN (@Industry)
+UNION ALL
+SELECT ' All'
+ORDER BY cCorpID ASC
+--site
+SELECT DISTINCT csiteno + ' | ' + ccompname1  AS [SiteName],csiteno[ClientKey]
+FROM [dbo].[Client]
+WHERE cgrpno IN (@Client)
+AND clnofbus IN (@Industry)
+AND (ccorpid IN (@CorpID) OR ' All' IN( @CorpID) )
+ORDER BY  SiteName 
